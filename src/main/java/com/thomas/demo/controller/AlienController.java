d7334fe5c313d53fbab6cf671a188f17a2f8bd39
thomaskovoor <thomaskovoor123@gmail.com>
2023-10-12 22:02:18 +0530

    Changed to Rest-CRUD operations implemented

--- src/main/java/com/thomas/demo/controller/AlienController.java
+++ src/main/java/com/thomas/demo/controller/AlienController.java
@@ -1,61 +1,59 @@
 package com.thomas.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.thomas.demo.dao.AlienRepo;
 import com.thomas.demo.model.Alien;
 
-@Controller
+@RestController
 public class AlienController {
 	
-	@GetMapping("/")
-	public String home() {
-		return "home.jsp";
-		
-	}
-	
-	@Autowired
-	AlienRepo repo;
-	
-	@PostMapping("/addAlien")
-	public String addAlien( Alien alien) {
-		
-		repo.save(alien);
-		
-		return "home.jsp";
-	}
-	
-	@GetMapping("/getAlien")
-	public ModelAndView getAlien(Integer alienId) {
-		
-		ModelAndView mv = new ModelAndView("ShowAlien.jsp");
-		Alien alien;
-		alien = repo.findById(alienId).orElse( new Alien());
-		mv.addObject(alien);
-		
-		//System.out.println(repo.findByAlienPlanet("Jupiter"));
-		//System.out.println(repo.findByAlienIdGreaterThan(5));
-		System.out.println(repo.findByPlanetSorted("Earth"));
-		return mv;
-		
-	}
-	@GetMapping("/removeAlien")
-	public String deleteAlien(Integer alienId)  {
-		
-		repo.deleteById(alienId);
-		
+@Autowired
+AlienRepo repo;
 
-		return "home.jsp";
-		}
-	@PostMapping("/updateAlien")
-	public String updateAlien(Alien alien) {
-		repo.save(alien);
-		return "home.jsp";
-		
+	@GetMapping(path = "/aliens"  /*,produces = "application/xml" for setting type of data returned */)
+	public List<Alien> getAllAliens()
+	{
+		return repo.findAll();
+	}
+
+	@GetMapping("/aliens/{alienId}")
+	//@ResponseBody no need to mention it if using restcontroller annotation else if using controller annotation use response body annotation
+	public Optional<Alien> getAlien(@PathVariable("alienId") int alienId){
+		return repo.findById(alienId);
+	}
+
+	@PostMapping("/aliens")
+	public Alien addAlien(@RequestBody Alien alien){
+           repo.save(alien);
+		   return alien;
+	}
+
+	@DeleteMapping("/aliens/{alienId}") 
+	public Alien deleteAlien(@PathVariable int alienId){
+          
+          Alien alien = repo.findById(alienId).orElse(new Alien());
+
+		  repo.delete(alien);
+		  return alien;
+
+	}
+	
+	@PutMapping(path="/aliens/{alienId}", produces = "application/json")
+	public Alien saveOrUpdateAlien(@PathVariable int alienId, @RequestBody Alien alien){
+       repo.save(alien);
+	   return alien;
+
 	}
 	
 	
